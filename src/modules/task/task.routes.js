a368d3c249b935e448366d48e1ef568361dbc403
Zyad Amr <Zyadamr.peronal@gmail.com>
2025-02-16 20:48:00 +0200

    task module auth

--- src/modules/task/task.routes.js
+++ src/modules/task/task.routes.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import taskController from './task.controller.js'
 import { getUploader } from '../../utils/upload.js'
+import { isAuthorized } from '../../middlewares/authorization.middleware.js'
+import { isAuthenticated } from '../../middlewares/authentication.middleware.js'
 
 const router = express.Router()
+
+router.get('/tasks', taskController.fetchTasks)
+
+router.get('/task/:taskID', taskController.fetchTask)
+
+router.use(isAuthenticated)
+router.use(isAuthorized('Chair', 'Director'))
 
 router.post('/upload-material/pdfs', getUploader('pdf', 'local').array('pdfs') ,taskController.uploadSessionPdfs)
 
 router.post('/upload-material/videos', getUploader('video', 'cloud').array('videos') ,taskController.uploadSessionVideos)
 
 router.post('/upload-material/images', getUploader('image', 'local').array('images') ,taskController.uploadSessionImages)
-
-router.get('/tasks', taskController.fetchTasks)
-
-router.get('/task/:taskID', taskController.fetchTask)
 
 router.patch('/task/:taskID', taskController.updateTask)
 
